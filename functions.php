c895a1ddae633dd338e5208347172d85cc890b16
Chris Van Patten <chris@vanpattenmedia.com>
2012-04-22 19:00:18 -0400

    SCSS cleanup, new title function replacing wp_title, etc.

--- functions.php
+++ functions.php
@@ -67,9 +67,31 @@ add_action('wp_before_admin_bar_render', 'my_admin_bar_remove');
 $breadcrumbs = 'off';
 
 // WP title fanciness
-function vpm_custom_title( ) {
+function vpm_custom_title() {
+	$separator = ' - ';
+
+	// Check for a slogan, and make sure it's not set to the default
+	if ( (get_bloginfo('description') != 'Just another WordPress site') ) {
+		$theslogan = ': ' . get_bloginfo('description');
+	} else { $theslogan = ''; }
+
+	// Some title conditionals
 	if ( is_front_page() ) {
-		return 'Van Patten Media';
+		$thetitle = get_bloginfo('name');
+	} elseif ( is_page() ) {
+		$thetitle = get_the_title() . $separator . get_bloginfo('name');
+	} elseif ( is_single() ) {
+		$thetitle = get_the_title() . $separator . get_bloginfo('name');
+	} elseif ( is_archive() ) {
+		$thetitle = post_type_archive_title('', false);
+	}
+
+	// For SEO, we check if it's less than 70 before we tack on your slogan.
+	// (change if you dare...)
+	if ( ( strlen($theslogan) + strlen($thetitle) ) <= 70 ) {
+		return $thetitle . $theslogan;
+	} else {
+		return $thetitle;
 	}
 }
 add_filter( 'wp_title', 'vpm_custom_title', 20 );
