c06a3471a01d05e1edb27188ee6fd106e4812ce7
Chris Van Patten <chris@vanpattenmedia.com>
2012-05-05 21:08:28 -0400

    Finish up the buttons, start to build out more of the project page. Margin on slide headers when necessary

--- single-project.php
+++ single-project.php
@@ -2,11 +2,14 @@
 		<article>
 			<div id="project-<?php the_ID(); ?>" <?php post_class('article'); ?>>
 				<?php vpm_content_header(); ?>
-				<div class="four columns" id="project-meta">
-					<a href="#" class="download-button">Download now</a>
-				</div>
-				<div class="entry-content">
-					<?php the_content(); ?>
+				<div class="row">
+					<div class="four columns omega" id="project-meta">
+						<a href="#" class="download-button">Download now</a>
+						<em id="version-info"><strong>Version 1.0</strong> &#8212; Requires WordPress 3.3.1 or greater</em>
+					</div>
+					<div class="entry-content" id="project-content">
+						<?php the_content(); ?>
+					</div>
 				</div>
 			</div>
 		</article>
