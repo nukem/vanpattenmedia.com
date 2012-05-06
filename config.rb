cff1b1d879834ab54f3977089a63bafd2d9b2548
Chris Van Patten <chris@vanpattenmedia.com>
2012-05-05 21:07:58 -0400

    Remove unnecessary upload stuff in config.rb

--- config.rb
+++ config.rb
@@ -1,6 +1,4 @@
 # Require
-require 'net/ssh'
-require 'net/sftp'
 require 'rgbapng'
 
 # Config
@@ -8,10 +6,6 @@ css_dir			= "css"
 sass_dir		= "sass"
 images_dir		= "img"
 javascripts_dir	= "js"
-
-# WordPress paths: No trailing slashes
-wordpress_root	= '/home/vanpattenmedia/staging/vanpattenmedia.com'
-rel_theme_path 	= '/content/themes/vanpattenpress'
 
 # Development
 # output_style	= :expanded
@@ -22,18 +16,4 @@ line_comments	= false
 output_style	= :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# Connection Details
-sftp_host = 'vanpattenmedia.com'
-sftp_user = 'vanpattenmedia'
-
-# Upload
-on_stylesheet_saved do |filename|
-	$local_path_to_css_file = css_dir + '/' + File.basename(filename)
-
-	Net::SFTP.start( sftp_host, sftp_user ) do |sftp|
-		sftp.upload! $local_path_to_css_file, wordpress_root + rel_theme_path + '/' + css_dir + '/' + File.basename(filename)
-	end
-	puts ">>> File uploaded."
-end
+relative_assets = true
