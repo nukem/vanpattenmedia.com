15fb1cdefc35052d246a0c599a65ec5d2ee46f46
Chris Van Patten <chris@vanpattenmedia.com>
2012-05-05 22:08:07 -0400

    cleanup in projects, add new row for contributions, and begin header alerts

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 	<header id="header" class="clearfix">
 		<div class="container">
-			<div class="sixteen columns">
+			<div class="sixteen columns" id="header-inner">
+				<div id="alert-announcement">
+					<p>We just released a new WordPress plugin that makes sliders awesome. <a href="#" class="button">Check it out...</a></p>
+				</div>
 				<a href="<?php echo home_url(); ?>" id="logo"><img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="Van Patten Media logo" id="vpm-logo"></a>
 			</div>
 		</div>
--- single-project.php
+++ single-project.php
@@ -7,11 +7,17 @@
 						<p><a href="http://wordpress.org/extend/plugins/ssl-subdomain-for-multisite/" id="button-download">Download now</a></p>
 						<p><em id="project-version-info"><strong>Version 1.0</strong> &#8212; Released <time datetime="2012-04-28T20:00Z"><abbr title="Saturday, April 28, 2012 at 20:00 UTC" id="project-fuzzy-release-date">one week ago</abbr></time></em></p>
 						<p><em id="project-requirements">Requires WordPress 3.3.1 or greater</em></p>
-						<p><a href="#" id="button-contribute">Contribute today!</a></p>
+						<p><a href="#contribute" id="button-contribute">Contribute today!</a></p>
 					</div>
 					<div class="entry-content" id="project-content">
 						<?php the_content(); ?>
 					</div>
+				</div>
+				<div class="row">
+					<h3><a name="contribute"></a>Contribute</h3>
+					<div class="four columns alpha">Donate</div>
+					<div class="four columns">Pull Requests</div>
+					<div class="four columns omega">Bug reports and feature requests</div>
 				</div>
 			</div>
 		</article>
