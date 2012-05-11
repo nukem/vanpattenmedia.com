f902c78925befd330ddb42414a012002bf220929
Chris Van Patten <chris@vanpattenmedia.com>
2012-05-10 20:39:14 -0400

    Production config.rb

--- config.rb
+++ config.rb
@@ -8,12 +8,12 @@ images_dir		= "img"
 javascripts_dir	= "js"
 
 # Development
-output_style	= :expanded
-line_comments	= true
+# output_style	= :expanded
+# line_comments	= true
 
 # Production
-#line_comments	= false
-#output_style	= :compressed
+line_comments	= false
+output_style	= :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
