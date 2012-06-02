6a7be828404c1058a218fa20e0704bc12d895b80
Chris Van Patten <chris@vanpattenmedia.com>
2012-06-02 16:36:25 -0400

    New version of production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,3 +5,26 @@ default_run_options[:pty] = true
 
 # Deploy path
 set :deploy_to, "/home/vanpattenmedia/vanpattenmedia.com"
+
+# nginx config
+set :domain_name, "vanpattenmedia.com"
+set :wp_theme_name, "vanpattenpress"
+set :staging, false
+
+after "deploy:check", "nginx:config"
+after "deploy", "nginx:reload"
+
+namespace :nginx do
+	task :config do
+		nginx_config = ERB.new(File.read("./config/deploy/templates/nginx.erb")).result(binding)
+		put nginx_config, "#{deploy_to}/shared/#{application}"
+		#run "#{sudo} mv #{deploy_to}/shared/#{application} /etc/nginx/sites-available/#{application}"
+		#run "#{sudo} chown root:root /etc/nginx/sites-available/#{application}"
+		#run "#{sudo} ln -s /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}"
+		#run "#{sudo} chown root:root /etc/nginx/sites-enabled/#{application}"
+	end
+
+	task :reload do
+		run "#{sudo} nginx -s reload"
+	end
+end
