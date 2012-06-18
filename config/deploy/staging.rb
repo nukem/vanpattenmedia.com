1f5bd48dea53bb9a623976c206c78045193223a0
Chris Van Patten <chris@vanpattenmedia.com>
2012-06-18 11:16:46 -0400

    - Fix for erb paths in site.pp.erb - Load appropriate database credentials

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,20 +3,22 @@ server "50.116.59.75", :app, :web, :db, :primary => true
 ssh_options[:port]        = 9012
 default_run_options[:pty] = true
 
+# Set stage
+set :app_stage,  "staging"
+
 project  = YAML.load_file("./config/project.yml")
 database = YAML.load_file("./config/database.yml")
 
-set :db_name,     database['dev']['name']
-set :db_user,     database['dev']['user']
-set :db_password, database['dev']['password']
-set :db_host,     database['dev']['host']
-set :db_grant_to, database['dev']['grant_to']
+set :db_name,     database[fetch(:app_stage)]['name']
+set :db_user,     database[fetch(:app_stage)]['user']
+set :db_password, database[fetch(:app_stage)]['password']
+set :db_host,     database[fetch(:app_stage)]['host']
+set :db_grant_to, database[fetch(:app_stage)]['grant_to']
 
 # nginx config
 set :app_name,   project['application']['name']
 set :app_theme,  project['application']['theme']
 set :app_theme,  "vanpattenpress"
-set :app_stage,  "staging"
 set :app_domain, "#{app_stage}." + project['application']['domain']
 
 # Deploy path
@@ -29,10 +31,11 @@ namespace :puppet do
   desc "Set up puppet"
   task :show, :roles => :app do
     run "mkdir -p /home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}"
+    upload("./config/puppet/templates", "/home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/templates")
 
     puppet_manifest = ERB.new(File.read("./config/puppet/templates/site.pp.erb")).result(binding)
     put puppet_manifest, "/home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/site.pp"
 
-   # run "#{sudo} puppet apply /home/#{fetch(:user)}/tmp/#{fetch(:app_name)}-#{fetch(:app_stage)}.pp"
+    run "#{sudo} puppet apply /home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/site.pp"
   end
 end
