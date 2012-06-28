360a82186da3b333743be4d000b394de0756e9ea
Chris Van Patten <chris@vanpattenmedia.com>
2012-06-28 17:28:35 -0400

    Moved to a devel branch, deleted a bunch of unnecessary rgbapng files, added coachmenyc screenshots, beginnings of quote functionality

--- public/content/themes/vanpattenpress/front-page.php
+++ public/content/themes/vanpattenpress/front-page.php
@@ -1,17 +1,19 @@
 <div class="sixteen columns" id="feature-box">
 	<h2 id="slogan">Beautiful responsive websites that help your business succeed online, so it flourishes offline.</h2>
 	<div id="responsive-demo">
-		<div id="laptop-demo"><img src="<?php bloginfo('template_directory'); ?>/img/homepage/responsive-screenshots/jeremyyaddaw/laptop.png" alt=""></div>
-		<div id="tablet-demo"><img src="<?php bloginfo('template_directory'); ?>/img/homepage/responsive-screenshots/jeremyyaddaw/tablet.png" alt=""></div>
-		<div id="smartphone-demo"><img src="<?php bloginfo('template_directory'); ?>/img/homepage/responsive-screenshots/jeremyyaddaw/smartphone.png" alt=""></div>
+		<div id="laptop-demo"><img src="<?php bloginfo('template_directory'); ?>/img/homepage/responsive-screenshots/coachmenyc/laptop.png" alt=""></div>
+		<div id="tablet-demo"><img src="<?php bloginfo('template_directory'); ?>/img/homepage/responsive-screenshots/coachmenyc/tablet.png" alt=""></div>
+		<div id="smartphone-demo"><img src="<?php bloginfo('template_directory'); ?>/img/homepage/responsive-screenshots/coachmenyc/smartphone.png" alt=""></div>
 	</div>
 </div>
 
-<div class="four columns nav">
-	<?php get_sidebar( $base ); ?>
-</div>
 <div class="twelve columns">
-	<section class="slider">
+	<section id="featured-quote">
+		<blockquote><span>Quisque ut vehicula neque. Cras eros odio, pharetra gravida dignissim sed, congue sit amet orci. Maecenas posuere leo in urna feugiat pellentesque. Nam eget nisl in est commodo mattis.</span></blockquote>
+		<cite>&mdash; John Doe</cite>
+	</section>
+
+	<!-- <section class="slider">
 		<?php
 			// Set up a new WP_Query
 			$my_args = array(
@@ -21,12 +23,12 @@
 			);
 			vpm_wp_query_slider($my_args, 'top');
 		?>
-	</section>
+	</section> -->
 
 	<div class="content">
-		<div class="entry-content">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif; ?>
-		</div>
+		<!-- <div class="entry-content">
+			<br><br><br>
+		</div> -->
 
 		<div class="row">
 			<section class="six columns alpha homebox">
@@ -54,4 +56,15 @@
 			</section>
 		</div>
 	</div>
+</div>
+
+<div class="four columns" id="nav-wrap">
+	<nav id="nav">
+		<div id="menus">
+			<?php wp_nav_menu( array(
+				'menu' => 'primary-menu',
+				'container' => 'false'
+			)); ?>
+		</div>
+	</nav>
 </div>
