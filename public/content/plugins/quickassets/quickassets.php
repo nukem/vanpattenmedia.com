36400e08ff6cc56f35e1cd326ed99d875498fe08
Chris Van Patten <chris@vanpattenmedia.com>
2012-07-03 12:51:30 -0400

    Implement quickassets and add in a (broken) nginx rule

---
+++ public/content/plugins/quickassets/quickassets.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: QuickAssets
+Plugin URI: http://www.vanpattenmedia.com/
+Description: QuickAssets URLs
+Author: Van Patten Media
+Version: 1.0
+Author URI: http://www.vanpattenmedia.com/
+*/
+
+include_once plugin_dir_path( __FILE__ ) . '../../../../vendor/php/quickassets/lib.php';
+$a = new QuickAsset();
+
+$a->addAssetType('img', array(
+	'assetPath' => 'img/',
+));
+
+$a->addAssetType('js', array(
+	'assetPath' => 'js/',
+));
+
+$a->addAssetType('css', array(
+	'assetPath' => 'css/',
+	'rootPath'  => plugin_dir_path( __FILE__ ) . '../../themes/vanpattenpress',
+));
+
+$a->addHost('/', array(
+	'assetTypes' => 'img, js, css',
+));
