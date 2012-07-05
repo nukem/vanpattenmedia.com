d214b234cfa417f19e15975c3f2dbd4c4474a48d
Chris Van Patten <chris@vanpattenmedia.com>
2012-07-05 13:52:33 -0400

    Removed legacy stuff from the old alert system, tweaking the hosting stuff

--- public/content/themes/vanpattenpress/page-hosting.php
+++ public/content/themes/vanpattenpress/page-hosting.php
@@ -1,10 +1,10 @@
-<?php vpm_content_header(); ?>
+<div class="sixteen columns"><?php vpm_content_header(); ?></div>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="seven columns">
+	<div class="six columns">
 		<div id="notepad"><?php the_content(); ?></div>
 	</div>
 <?php endwhile; endif; ?>
 
-<div class="nine columns">
+<div class="ten columns">
 	plans
 </div>
