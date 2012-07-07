c09bec372ee2cf9d765acd8983a22b87fc010cfc
Peter Upfold <strategyoracle+github@gmail.com>
2012-07-07 10:06:23 +0100

    Further TimThumb, hardening, make timthumb.php non-externally accessible

--- public/content/themes/vanpattenpress/page-portfolio.php
+++ public/content/themes/vanpattenpress/page-portfolio.php
@@ -31,9 +31,8 @@
 				foreach ($the_category as $category) {
 					$cat_ID = $category->cat_ID;
 				}
-
 				// Output all the things!
-				echo '<div id="slide-' . get_the_ID() . '" '; post_class('slide header-top'); echo ' style="background: url(' . get_bloginfo('template_directory') . '/img/timthumb/timthumb.php?src=' . $full_url[0] . '&w=700&f=2|8|8|8|8|8|8|8|8)">' . "\n";
+				echo '<div id="slide-' . get_the_ID() . '" '; post_class('slide header-top'); echo ' style="background: url(' . get_bloginfo('template_directory') . '/img/e:grayblur/b:8/w:700/' . preg_replace('#^http://#i', '', $full_url[0]) . '">' . "\n";
 
 						echo '<a href="' . get_permalink() . '">' . "\n";
 
