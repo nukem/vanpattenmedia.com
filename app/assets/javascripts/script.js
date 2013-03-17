0b8ab928443cf47330268e8102ded459a02a5b80
Chris Van Patten <chris@vanpattenmedia.com>
2013-03-17 12:27:56 -0400

    Remove jQuery/JS tag

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 
 	var window_height = $(window).height();
 
-	$('.home .primary').css( 'margin-top', window_height - 600 );
+//	$('.home .primary').css( 'margin-top', window_height - 600 );
 
 });
