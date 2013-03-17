f9063bbdc93702350aff60534aa5d1fa28d11f74
Chris Van Patten <chris@vanpattenmedia.com>
2013-03-17 12:15:23 -0400

    Media queries! Beginning of intrapost navigation!

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 
 	var window_height = $(window).height();
 
-	$('.home-spacing').css( 'padding-top', window_height - 490 );
+	$('.home .primary').css( 'margin-top', window_height - 600 );
 
 });
