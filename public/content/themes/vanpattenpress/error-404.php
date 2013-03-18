e0b6d5e987667f2d38e4fc27b55817e986fbc5f2
Chris Van Patten <chris@vanpattenmedia.com>
2013-03-18 14:54:57 -0400

    Remove unnecessary shtuff

--- public/content/themes/vanpattenpress/error-404.php
+++
@@ -1,8 +0,0 @@
-<article>
-	<div>
-		<h2>Page not found</h2>
-		<div class="entry-content">
-			<p>We couldn&rsquo;t find the page you&rsquo;re looking for. Sorry!</p>
-		</div>
-	</div>
-</article>
