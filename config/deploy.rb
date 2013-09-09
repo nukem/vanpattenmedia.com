40b1df33857901fb08575759d258b8ebce158379
Chris Van Patten <chris@vanpattenmedia.com>
2013-09-09 19:39:17 -0400

    Attempt to get everything working again... facepalm

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,14 +18,17 @@ set :user,             project['deploy_user']
 set :app_user,         project['user']
 set :app_group,        project['group']
 set :app_access_users, project['access_users']
+set :app_theme,        project['theme']
 set :repository,       project['repo']
 set :site_domain,      project['domain']
 
 # Load vpmframe requirements
 require 'vpmframe/erb-render'
 require 'vpmframe/capistrano/assets'
+require 'vpmframe/capistrano/puppet'
 require 'vpmframe/capistrano/credentials'
 require 'vpmframe/capistrano/permissions'
+require 'vpmframe/capistrano/wp-salts'
 
 # Don't do Railsy things...
 namespace :deploy do
@@ -38,51 +41,6 @@ namespace :deploy do
   end
 end
 
-namespace :jekyll do
-  desc "Compile the jekyll site"
-  task :compile, :roles => :app do
-	# Fonts
-    system("cp -R ~/.captemp/#{fetch(:application)}/app/assets/fonts/ ~/.captemp/#{fetch(:application)}/raw/fonts")
-
-    # JavaScript
-    system("cd ~/.captemp/#{fetch(:application)} && make assets")
-
-	# Go Jekyll Go
-    system("cd ~/.captemp/#{fetch(:application)} && jekyll")
-  end
-
-  desc "Upload the jekyll site"
-  task :upload, :roles => :app do
-    system("scp -r -P #{fetch(:app_port)} ~/.captemp/#{fetch(:application)}/public #{fetch(:user)}@#{fetch(:app_server)}:#{release_path}/")
-  end
-end
-
-namespace :puppet do
-
-  desc "Set up puppet"
-  task :show, :roles => :app do
-    # Remove any existing directories, so we can reupload them
-    run "rm -rf /home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}"
-
-    # Make the directory again
-    run "mkdir -p /home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}"
-
-    # Upload the configurations
-    upload("./config/erb-render.rb", "/home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/erb-render.rb", :via => :scp)
-    upload("./config/puppet", "/home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}", :via => :scp, :recursive => :true)
-    upload("./config/nginx", "/home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/nginx", :via => :scp, :recursive => :true)
-    upload("./config/varnish", "/home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/varnish", :via => :scp, :recursive => :true)
-
-    # Render the manifest
-    puppet_manifest = ERB.new(File.read("./config/puppet/site.pp.erb")).result(binding)
-    put puppet_manifest, "/home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/site.pp"
-
-    # Apply the manifest
-    run "#{sudo} puppet apply /home/#{fetch(:user)}/tmp/#{fetch(:app_name)}/#{fetch(:app_stage)}/site.pp"
-  end
-
-end
-
 # Setup related tasks
 before "deploy:setup",
   "puppet:show"
@@ -93,10 +51,10 @@ after "deploy:setup",
 # Compile and upload assets
 before "deploy",
   "assets:local_temp_clone",
-  "jekyll:compile"
+  "assets:compile_local_assets"
 
 before "deploy:create_symlink",
-  "jekyll:upload"
+  "assets:upload_local_assets"
 
 # Fix ownership
 before "deploy:restart",
