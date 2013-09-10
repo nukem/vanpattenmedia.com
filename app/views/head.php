cd43116e0a08dc57bec57acde2239dac503de8a4
Chris Van Patten <chris@vanpattenmedia.com>
2013-09-10 16:07:49 -0400

    add a meta description tag

--- app/views/head.php
+++ app/views/head.php
@@ -4,7 +4,9 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
 	<title>Van Patten Media</title>
+	<meta name=description" content="We build high-calibre responsive websites and web applications (usually with WordPress). Meet us, see our work, and get in touch.">
 
 	<script type="text/javascript">
 		(function() {
