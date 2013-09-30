73e05b001bcac25cf2a150577badf51978b4a3af
Francis <my_junk_mail_also@yahoo.com>
2013-09-30 08:13:04 +0800

    Spam filter using Akismet

--- public/thank-you/index.php
+++ public/thank-you/index.php
@@ -1,7 +1,41 @@
 <?php
 
+define("AKISMET_API_KEY", "2b0eecedacaa");
+require_once('../../app/vendor/akismet/akismet.class.php');
+
+
 if(isset($_POST['name'], $_POST['email'], $_POST['message'])) {
-	email($_POST);
+
+	$comment = array(
+			'author' => $_POST['name'],
+			'email' => $_POST['email'],
+			'body' => $_POST['message'],
+			'permalink' => 'http://'.$_SERVER['HTTP_HOST'],
+	);
+
+	$akismet = new Akismet('http://'.$_SERVER["HTTP_HOST"], AKISMET_API_KEY, $comment);
+
+	if($akismet->errorsExist()) { // Returns true if any errors exist.
+			if($akismet->isError('AKISMET_INVALID_KEY')) {
+					// Do something. Log the error or email the user
+					error_log('Akismet invalid key!', 0);
+			} elseif($akismet->isError('AKISMET_RESPONSE_FAILED')) {
+					// Do something.
+					error_log('Akismet response failed', 0);
+			} elseif($akismet->isError('AKISMET_SERVER_NOT_FOUND')) {
+					// Do something.
+					error_log('Akismet server not found!', 0);
+			}
+	} else {
+			// No errors, check for spam.
+			if ($akismet->isSpam()) { // Returns true if Akismet thinks the comment is spam.
+					// Do something with the spam comment.
+					echo 'SPAM!';
+			} else {
+					// Do something with the non-spam comment.
+					email($_POST);
+			}
+	}
 }
 
 function email($array) {
